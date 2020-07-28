4cfd075c71ac177d9a12d1c37758918a4ef8dccf
Zhenhan Li <bigbroase@163.com>
2020-07-28 00:59:09 -0700

    fix: judge already signed in status

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,32 @@
 
 const router = require('koa-router')()
 
+/**
+ * retrieve user info
+ * @param {Object} ctx ctx
+ */
+function getLoginInfo(ctx) {
+    let data = {
+        isLogin: false // default not login
+    }
+
+    const userInfo = ctx.session.userInfo 
+    console.log('userInfo: ', userInfo)
+    if (userInfo) {
+        data = {
+            isLogin: true,
+            userName: userInfo.userName
+        }
+    }
+    return data
+}
+
 router.get('/login', async (ctx, next) => {
-    await ctx.render('login', {})
+    await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-    await ctx.render('register', {})
+    await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
