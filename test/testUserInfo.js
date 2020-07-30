df7dc3932481c4c72159724e87d28bbe5bb3be35
Zhenhan Li <bigbroase@163.com>
2020-07-29 18:24:11 -0700

    feature: profile unit test

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -8,5 +8,6 @@
  * Every time test user, need to update the cookie below
  */
 module.exports = {
+    USER_NAME: 'ciri',
     COOKIE : 'weibo.sid=aY58xK3zUkYqhqKAJF7Ob72xatYYD_pJ; weibo.sid.sig=3EVie9Pn4SVRmv1wdn-qlNvrbZk'
 }
